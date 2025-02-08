123184272f0ea58621edb0e48c1d2ea42cc3bfa1
mateusobma <mateusdeoliveirabarbosa@live.com>
2025-02-08 15:59:52 -0300

    Subindo para o github

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+document.getElementById("aumentar-texto").addEventListener("click", function() {
+    const body = document.body;
+    let currentFontSize = window.getComputedStyle(body).fontSize;
+    currentFontSize = parseFloat(currentFontSize);
+
+    // Aumenta a fonte
+    body.style.fontSize = (currentFontSize * 1.2) + "px";
+});
+
+document.getElementById("diminuir-texto").addEventListener("click", function() {
+    const body = document.body;
+    let currentFontSize = window.getComputedStyle(body).fontSize;
+    currentFontSize = parseFloat(currentFontSize);
+
+    // Aumenta a fonte
+    body.style.fontSize = (currentFontSize / 1.2) + "px";
+});
